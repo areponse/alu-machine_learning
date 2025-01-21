d4ccb86643dba265af442135211e291949826985
Reponse <reponseashime@gmail.com>
2025-01-21 13:07:21 +0200

    Advanced

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -3,10 +3,6 @@
 Slice numpy arrays
 """
 
-
-import numpy as np
-
-
 def np_slice(matrix, axes=None):
     """Slices a matrix along specific axes
 
@@ -28,12 +24,3 @@ def np_slice(matrix, axes=None):
         else:
             slices.append(slice(None))
     return matrix[tuple(slices)]
-  
-mat1 = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
-print(np_slice(mat1, axes={1: (1, 3)}))
-print(mat1)
-# mat2 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-#                  [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]],
-#                  [[21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]])
-# print(np_slice(mat2, axes={0: (2,), 2: (None, None, -2)}))
-# print(mat2)
